3deef9cb705ad2633254313d4f983379ece267ff
tasnime <tasnimsahli97@gmail.com>
2025-09-22 09:31:07 +0100

    creat mon projet

---
+++ Décompteur de compétences Solicode/script.js
@@ -0,0 +1,13 @@
+function number() {
+    let table=["c1,c3,c1;c2"];
+    let choisi="c1";
+    let conteur=0;
+    for (let i =0; i<table.length; i++) {
+    if (table[i]===element) {
+        conteur++;
+    }
+    }
+   console.log("le nombre totale est" + conteur);
+ 
+}
+nombre();
